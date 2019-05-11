6583625f335442847e9798e6e4d4bd91207a2ddf
Genaro Belen <gbelen@gmail.com>
2019-05-11 06:00:22 -0400

    Realizada la conexion con Firebase y muestra de registros Clientes en el componente Clientes

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoappService } from 'src/app/services/infoapp.service';
 
 @Component({
   selector: 'app-clientes',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+	constructor( public _servicioClientes: InfoappService) {
+		
+	}
 
   ngOnInit() {
   }
--- src/app/services/infoapp.service.ts
+++ src/app/services/infoapp.service.ts
@@ -10,17 +10,40 @@ export class InfoappService {
 	info: InfoAppInterface = {};
 	loaded = false;
 
+	clientes: any[] = [];
+
 	constructor(private http: HttpClient ) { 
 
 		//console.log('Estoy iniciando un servicio para sacar la info del app creado en assets data.');
+		this.getInfoApp();
+		this.getClientsList();
 
+	}
+
+
+	private getInfoApp(){
 		//Leo el archivo json que cree previamente en assets/data
 		this.http.get('assets/data/info-app.json')
 			.subscribe( (resp: InfoAppInterface) =>{
-				this.loaded = true; //
+				this.loaded = true; 
 				this.info = resp;
+		});
+	}
+	
+	
+	//Function que obtiene el listado de clientes en la BD
+	private getClientsList(){
+
+		
+		this.http.get('https://ziba-70342.firebaseio.com/cliente.json')
+			.subscribe( (resp: any[]) =>{
+				
+				this.clientes = resp;
 				console.log(resp);
-		})
+		});
 
 	}
+
+
+
 }
