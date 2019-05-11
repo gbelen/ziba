c16680bbfaeb8582ea1c706b70a7549fd69ad272
Genaro Belen <gbelen@gmail.com>
2019-05-11 04:15:10 -0400

    Agregué algunas rutas al NavBar en el componente header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ClientesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+
+
+}
